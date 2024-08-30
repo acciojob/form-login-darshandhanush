2d4cb0f4b58c7c8e19daf49df0a9fef5fefe0d99
darshandhanush <146746929+darshandhanush@users.noreply.github.com>
2024-08-30 16:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getFormvalue() {
+function getFormvalue(event) {
 	event.preventDefault();
    	var name=document.getElementById('fname').value;
 	var last=document.getElementById('lname').value;
