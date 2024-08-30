91a2ffa40bf76958d3fdab04ce763b89e5e00491
darshandhanush <146746929+darshandhanush@users.noreply.github.com>
2024-08-30 15:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById("fname").value
+	const lastName = document.getElementById("lnamr").value
+
+	alert(fname+lanme)
+	
 
 }
