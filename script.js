fc167f6a62a74040714dbb7853ef2e7cccd70e83
darshandhanush <146746929+darshandhanush@users.noreply.github.com>
2024-08-30 16:33:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getFormvalue() {
    	var name=document.getElementById('fname').value;
 	var last=document.getElementById('lname').value;
 
-	alert(name + last)
+	alert(name +" "+ last)
 	
 }
 
